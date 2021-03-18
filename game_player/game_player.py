00da7d48d7593afff3ddea7212240b05952d1ef7
Utkarsh S <utkarshcpark@gmail.com>
2021-03-18 23:57:18 +0530

    Add pyautogui

--- game_player/game_player.py
+++ game_player/game_player.py
@@ -1,3 +1,53 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+import cv2 
+import pyautogui
+import time
+import sys
+count=0
+cap=cv2.VideoCapture(cv2.CAP_DSHOW) 
+#cap.set(CV_CAP_PROP_FPS,25)
+tracker= cv2.TrackerCSRT_create()
+success,img= cap.read() 
+
+img = cv2.flip(img, 1)
+bbox= cv2.selectROI("tracking",img,False)
+tracker.init(img,bbox)
+def drawbox(img,bbox):
+    x,y,w,h= int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    global count
+    if(x>300 and y>160 and x<340 and y <320):
+        count=0
+    if(x>0 and y<160):
+            print("up")
+            pyautogui.press("up")
+            #pyautogui.keyDown('up')
+            #pyautogui.keyUp('up')
+    elif(x>0 and y>320):
+            print("down")
+            pyautogui.press("down")
+            #pyautogui.keyDown('down')
+            #pyautogui.keyUp('down')
+    if(count<1):
+        if(x>340 and y>160):
+            print("right")
+            pyautogui.press("right")
+            #pyautogui.keyDown('right')
+            #pyautogui.keyUp('right')
+            count+=1
+        elif(x<300 and y>160):
+            print("left")
+            pyautogui.press('left')
+            #pyautogui.keyDown('left')
+            #pyautogui.keyUp('left')
+            count+=1
+            #time.sleep(0.001)
+        
+        
+    cv2.circle(img,(x,y), 10,(255, 255, 0),3)
+
+while cap.isOpened():
     timer= cv2.getTickCount()
     success,img= cap.read()
     img = cv2.flip(img, 1)
@@ -15,3 +65,5 @@
     cv2.imshow("video",img)
     
     if cv2.waitKey(1) == 27 :break
+cap.release()
+cv2.destroyAllWindows()
